9b09dad37d291494687511187ebfe1f8c6e8fd43
Anmol Brar <brarsanmol@protonmail.com>
2020-09-17 13:57:54 -0400

    Add default gray skin (#4d4d4d) instead of allow client to render Steve and Alex Heads.

--- src/main/java/me/ceezuns/tablist/TablistSlot.java
+++ src/main/java/me/ceezuns/tablist/TablistSlot.java
@@ -1,12 +1,15 @@
 package me.ceezuns.tablist;
 
 import com.comphenix.protocol.wrappers.WrappedGameProfile;
+import com.comphenix.protocol.wrappers.WrappedSignedProperty;
 import com.google.common.base.Preconditions;
 import me.ceezuns.Poseidon;
 
 import java.util.UUID;
 
 public class TablistSlot {
+
+    private static final WrappedSignedProperty DEFAULT_SKIN = new WrappedSignedProperty("textures", "ewogICJ0aW1lc3RhbXAiIDogMTYwMDMwNTQ1NDg2NCwKICAicHJvZmlsZUlkIiA6ICI0ZDcwNDg2ZjUwOTI0ZDMzODZiYmZjOWMxMmJhYjRhZSIsCiAgInByb2ZpbGVOYW1lIiA6ICJzaXJGYWJpb3pzY2hlIiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2JmODNlNjk0NjYzMDVmYWZmMmFjNzYwNzZhZWE2ZGUyZjRiMzliMzkwOTkwMmUwNWJmMGU3NTBiM2U3YThhOCIKICAgIH0KICB9Cn0=", "aWIsgxVvNgGa8XVJK2+Ygh7K/M9QwYeOPsg7SDrVhbr6SjidbO1NLT68czbwfyriUdA7ZNhAkgW991tUlHMBnePmQOoe2Dr/OfE7AnVbkH4xV6iNMR3wEBaJd3AKiiZmRbWW2Fb7ZXHXIMjJQJSHBS7AhrRBj/Z7gajUIsQdDRUfM792sss1OiFyok4UrpPWMb6LpwHsqIK5D0jsGK4nu6bavtiJW7HYes4p7mQMKeAymWtOwBagq/UlXqFae6DdM8lpQUo6Nzdz+mFzcpxhxZzazMCK2nifVYBFKIFEUXZy8KOO/BE2Mbo35Pa3aEVoMLvkyqA8sNlihzqoxH5q7hgk4h/pb33Kn9yUF5tjkgJ5d42W+OVcSig7emYWjQafWJZTPF+WzWqV9fr9COESotNHbzJSkbKYHiFa/v1Go2XaS0G0tfczflXXqs5YL2sg0Wem2swpFQ3lvLEDwiwiKR7NSbj+3lcto//JJnG5ExcaGhwgrQ6pKUhBV5QdlDm5WE03Li8fbINsJTkzIhicjUBxhA21uN4OXOntKxSon3HB1wo4oG+xfvpKazcgJeJvv7V0LhZ9Ix62c6dKJ6FmMeWuspGGn/GePKvMREyIsMXCY7hVqDzZjLm6m5lsZb4GemS0mCElobumbClhSj2Q6hwbvTAjZYbCT6XELUAZgig=");
 
     private String text;
     private int latency;
@@ -17,7 +20,7 @@ public class TablistSlot {
         this.text = " ";
         this.latency = 149;
         this.profile = new WrappedGameProfile(UUID.randomUUID(), "!" + Poseidon.getInstance().getTablistKeyGenerator().getNextKey(index));
-        this.profile.getProperties().removeAll("textures");
+        this.profile.getProperties().put("textures", DEFAULT_SKIN);
         this.shouldUpdate = true;
     }
 
